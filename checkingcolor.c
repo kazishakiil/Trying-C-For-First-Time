b10ce597f5a8f87a849aa06e261671a125cd7472
Kazi Shakil <116259611+kazishakiil@users.noreply.github.com>
2022-10-22 15:15:09 +0600

    Add files via upload

---
+++ checkingcolor.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+system("COLOR 0B");
+int n,i;
+    printf("Enter a number to Repeat:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{ 
+		printf("I Love You \3\n ");
+	}
+}
